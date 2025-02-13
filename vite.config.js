a1292e95d846b3360c26d7d5a939f979973e805e
KTGamage <162305662+KTGamage@users.noreply.github.com>
2025-02-13 21:02:09 +0530

    vercel change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: 'dist',  // Explicitly set the output directory
+    emptyOutDir: true, // Ensures the 'dist' directory is emptied before a new build
+  }
+});
