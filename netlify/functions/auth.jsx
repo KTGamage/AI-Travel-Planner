cecd0e0afcbcf82b448133c9b59b40c4633a3a4a
KTGamage <kasuntharaka18628@gmail.com>
2025-09-08 14:20:07 +0530

    remove netlify auth function

--- netlify/functions/auth.jsx
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  // Handle OAuth redirect here
-  return {
-    statusCode: 302,
-    headers: {
-      Location: `https://goplan-ai-fe04d1.netlify.app/?token=${event.queryStringParameters.code}`
-    }
-  };
-};
